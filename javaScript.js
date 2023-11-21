089b1fde89fe64e817e7228e0e5ebffb245677ee
GiselleWandurraga <146407129+GiselleWandurraga@users.noreply.github.com>
2023-11-20 19:35:36 -0500

    actualización

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,4 @@
+
 const carrito = []
 const productos = [
     {
@@ -8,76 +9,70 @@ const productos = [
     },
     {
         id: 2,
-        nombre: "Chaqueta Azul",
+        nombre: "Sreep terracota",
         precio: 120000,
         img: "./imagenes/producto2.jpg"
     },
     {
         id: 3,
-        nombre: "Conjunto Deportivo Negro",
-        precio: 120000,
+        nombre: "Conjunto perla",
+        precio: 100000,
         img: "./imagenes/producto3.jpg"
     },
     {
         id: 4,
-        nombre: "Buzo algodonado gris",
-        precio: 170000,
+        nombre: "Vestido Negro y Blanco",
+        precio: 115000,
         img: "./imagenes/producto4.jpg"
     },
     {
         id: 5,
-        nombre: "Short Strip Azul",
-        precio: 110000,
+        nombre: "Oversise Blanco y Negro",
+        precio: 130000,
         img: "./imagenes/producto5.jpg"
     },
     {
         id: 6,
-        nombre: "Vestido perla",
+        nombre: "Vestido esmeralda",
         precio: 170000,
         img: "./imagenes/producto6.jpg"
     },
     {
         id: 7,
-        nombre: "Enterizo café",
+        nombre: "Vestido Negro",
         precio: 150000,
         img: "./imagenes/producto7.jpg"
     },
     {
         id: 8,
-        nombre: "Conjunto perlado",
-        precio: 250000,
+        nombre: "Elegant Black Dress",
+        precio: 180000,
         img: "./imagenes/producto8.jpg"
     },
     {
         id: 9,
-        nombre: "Conjunto perlado",
-        precio: 250000,
+        nombre: "Turquesa Dress",
+        precio: 90000,
         img: "./imagenes/producto9.jpg"
     },
     {
         id: 10,
-        nombre: "Conjunto perlado",
-        precio: 250000,
+        nombre: "Leopard",
+        precio: 135000,
         img: "./imagenes/producto10.jpg"
     },
-
     {
         id: 11,
-        nombre: "Conjunto perlado",
-        precio: 250000,
+        nombre: "Vestido Verde",
+        precio: 100000,
         img: "./imagenes/producto11.jpg"
     },
-
     {
         id: 12,
-        nombre: "Conjunto perlado",
-        precio: 250000,
+        nombre: "Vestido Verde Neón",
+        precio: 120000,
         img: "./imagenes/producto12.jpg"
     },
-
-
-
-
 ]
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -85,7 +80,6 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 function pintar() {
-
     let fragment = document.createDocumentFragment();
     productos.forEach((item, index) => {
         let div = document.createElement("div");
@@ -98,7 +92,7 @@ function pintar() {
         let button = document.createElement("button")
         button.textContent = "Agregar al carrito"
         button.addEventListener("click", () => {
-            agregar_carrito(productos.id)
+            agregar_carrito(item.id)
         })
         div.appendChild(img)
         div.appendChild(h2)
@@ -107,19 +101,117 @@ function pintar() {
         fragment.appendChild(div)
 
         div.className = "cards"
-        h2.className="nombre_articulo"
-        p.className="precio"
-        button.className="boton_agregar"
-
+        h2.className = "nombre_articulo"
+        p.className = "precio"
+        img.className = "img_contenedor"
+        button.className = "boton_agregar"
     })
     document.getElementById("cards").appendChild(fragment)
+}
 
+function agregar_carrito(id) {
+    const seleccionado = productos.find(producto => producto.id === id);
+
+    const producto_carrito = carrito.find(item => item.producto.id === id);
+
+    if (producto_carrito) {
+        producto_carrito.cantidad += 1;
+    } else {
+        carrito.push({
+            producto: {
+                id: seleccionado.id,
+                nombre: seleccionado.nombre,
+                precio: seleccionado.precio,
+                img: seleccionado.img
+            },
+            cantidad: 1
+        });
+    }
+    console.log(carrito);
+    pintar2()
 }
 
 
-function agregar_carrito(id){
-  const seleccionados = productos.find(producto => producto.id === id)
-  const productos_carrito= carrito.find (item => item.producto,id ===id)
-  carrito.push({producto: seleccionados, cantidad: 1})
-  console.log(carrito)
+
+function pintar2() {
+    let total = 0;
+    const tabla = document.getElementById("tabla");
+    tabla.innerHTML = "";
+
+    carrito.forEach(item => {
+        console.log(item);
+        const producto = item.producto;
+        const cantidad = item.cantidad;
+        const subtotal = producto.precio * cantidad;
+        total += subtotal;
+
+        let tr = document.createElement("tr");
+
+        let imgTabla = document.createElement("td");
+        let imgElement = document.createElement("img");
+        imgElement.src = producto.img;
+        imgTabla.appendChild(imgElement);
+
+
+        let nombreTabla = document.createElement("td");
+        nombreTabla.textContent = producto.nombre;
+
+        let precioTabla = document.createElement("td");
+        precioTabla.textContent = producto.precio.toLocaleString('es-CO', { style: 'currency', currency: 'COP' });
+        1
+        let cantidadTabla = document.createElement("td");
+        cantidadTabla.textContent = cantidad;
+
+        let subtotalTabla = document.createElement("td");
+        subtotalTabla.textContent = subtotal.toLocaleString('es-CO', { style: 'currency', currency: 'COP' });
+
+        let eliminarProducto = document.createElement("td");
+        let eliminar = document.createElement("button");
+        eliminar.textContent = "❌";
+        eliminar.addEventListener("click", () => {
+            eliminarRegistro(item.producto.id);
+        });
+        eliminarProducto.appendChild(eliminar);
+
+        tr.appendChild(imgTabla);
+        tr.appendChild(nombreTabla);
+        tr.appendChild(precioTabla);
+        tr.appendChild(cantidadTabla);
+        tr.appendChild(subtotalTabla);
+        tr.appendChild(eliminarProducto);
+        tabla.appendChild(tr);
+
+        nombreTabla.className = "nombre_tabla"
+
+    });
+
+    document.getElementById("tot").textContent = total.toLocaleString('es-CO', { style: 'currency', currency: 'COP' });
+    console.log(carrito);
+    console.log(total);
 }
+
+function abrirCarrito() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "block";
+}
+
+
+function cerrarCarrito() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
+
+function eliminarRegistro(productoId) {
+    const indice = carrito.findIndex((item) => item.producto.id === productoId);
+    if (indice !== -1) {
+        carrito.splice(indice, 1);
+        pintar2();
+    }
+}
+
+function vaciarCarrito() {
+    carrito.length = 0; 
+    pintar2(); 
+}
+
+
